1b9ffb9696962ed76713af50ccc2cba600fa2cbf
Auro Tripathy <ipserv@yahoo.com>
2016-10-18 09:04:33 -0700

    refactor1

--- examples/ssd/ssd_server_detect.py
+++ examples/ssd/ssd_server_detect.py
@@ -9,19 +9,6 @@ from os.path import expanduser
 from read_annotations import read_annotations
 from pudb import set_trace
 
-plt.rcParams['figure.figsize'] = (10, 10)
-plt.rcParams['image.interpolation'] = 'nearest'
-plt.rcParams['image.cmap'] = 'gray'
-
-# Make sure that caffe is on the python path:
-caffe_root = '.'  # this file is expected to be in {caffe_root}/examples/ssd
-import os
-os.chdir(caffe_root)
-import sys
-sys.path.insert(0, 'python')
-
-# Load LabelMap.
-#---------------
 import caffe
 caffe.set_device(0)
 caffe.set_mode_gpu()
@@ -29,15 +16,10 @@ caffe.set_mode_gpu()
 from google.protobuf import text_format
 from caffe.proto import caffe_pb2
 
-conf_threshold = 0.10
+import os
+import sys
 
-# load VOC labels for the 21 classes
-labelmap_file = 'data/VOC0712/labelmap_voc.prototxt'
-file = open(labelmap_file, 'r')
-labelmap = caffe_pb2.LabelMap()
-text_format.Merge(str(file.read()), labelmap)
-
-def get_labelname(labelmap, labels):
+def get_labelnames(labelmap, labels):
     num_labels = len(labelmap.item)
     labelnames = []
     if type(labels) is not list:
@@ -51,29 +33,6 @@ def get_labelname(labelmap, labels):
                 break
         assert found == True
     return labelnames
-
-
-# Load the net in the test phase for inference, and configure input preprocessing.
-#----------------------------------------------------------------------------------
-
-model_def = 'models/VGGNet/VOC0712/SSD_300x300/deploy.prototxt'
-model_weights = 'models/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300_iter_60000.caffemodel'
-
-net = caffe.Net(model_def,      # defines the structure of the model
-                model_weights,  # contains the trained weights
-                caffe.TEST)     # use test mode (e.g., don't perform dropout)
-
-# input preprocessing: 'data' is the name of the input blob == net.inputs[0]
-transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
-transformer.set_transpose('data', (2, 0, 1))
-transformer.set_mean('data', np.array([104,117,123])) # mean pixel
-transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
-transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
-
-# Section2
-# SSD based detection
-#---------------------
-# set net to batch size of 1
 
 def run_detect_net(image, gt_list):
     # Run the net and examine the top-k results
@@ -97,7 +56,7 @@ def run_detect_net(image, gt_list):
 
     top_conf = det_conf[top_indices]
     top_label_indices = det_label[top_indices].tolist()
-    top_labels = get_labelname(labelmap, top_label_indices)
+    top_labels = get_labelnames(labelmap, top_label_indices)
     top_xmin = det_xmin[top_indices]
     top_ymin = det_ymin[top_indices]
     top_xmax = det_xmax[top_indices]
@@ -124,21 +83,46 @@ def run_detect_net(image, gt_list):
         currentAxis.add_patch(plt.Rectangle(*coords, fill=False, edgecolor=color, linewidth=2))
         currentAxis.text(xmin, ymin, display_txt, bbox={'facecolor':color, 'alpha':0.5})
 
-    # plot the ground_truth
-    # for gt in gt_list:
-    #     xmin, xmax, ymin, ymax = gt
-    #     xmin = int(round(top_xmin[i] * image.shape[1]))
-    #     ymin = int(round(top_ymin[i] * image.shape[0]))
-    #     xmax = int(round(top_xmax[i] * image.shape[1]))
-    #     ymax = int(round(top_ymax[i] * image.shape[0]))
-    #     score = top_conf[i]
-    #     label = int(top_label_indices[i])
-    #     label_name = top_labels[i]
-    #     display_txt = ''
-    #     coords = (xmin, ymin), xmax-xmin+1, ymax-ymin+1
-    #     color = (0, 0, 0.5)
-    #     currentAxis.add_patch(plt.Rectangle(*coords, fill=False, edgecolor=color, linewidth=2))
-    #     currentAxis.text(xmin, ymin, display_txt, bbox={'facecolor':color, 'alpha':0.5})
+
+
+plt.rcParams['figure.figsize'] = (10, 10)
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+# Make sure that caffe is on the python path:
+caffe_root = '.'  # this file is expected to be in {caffe_root}/examples/ssd
+os.chdir(caffe_root)
+sys.path.insert(0, 'python')
+
+conf_threshold = 0.10
+
+# load VOC labels for the 21 classes
+labelmap_file = 'data/VOC0712/labelmap_voc.prototxt'
+file = open(labelmap_file, 'r')
+labelmap = caffe_pb2.LabelMap()
+text_format.Merge(str(file.read()), labelmap)
+
+
+
+# Load the net in the test phase for inference, and configure input preprocessing.
+model_def = 'models/VGGNet/VOC0712/SSD_300x300/deploy.prototxt'
+model_weights = 'models/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300_iter_60000.caffemodel'
+
+net = caffe.Net(model_def,      # defines the structure of the model
+                model_weights,  # contains the trained weights
+                caffe.TEST)     # use test mode (e.g., don't perform dropout)
+
+# input preprocessing: 'data' is the name of the input blob == net.inputs[0]
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+transformer.set_transpose('data', (2, 0, 1))
+transformer.set_mean('data', np.array([104,117,123])) # mean pixel
+transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
+transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
+
+# Section2
+# SSD based detection
+#---------------------
+# set net to batch size of 1
 
 
 image_resize = 300
