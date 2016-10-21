a950df0d8867182cac6bc7951034bad3c3b9ced6
Auro Tripathy <ipserv@yahoo.com>
2016-10-21 00:14:46 -0700

    working curl command /curl/

--- examples/ssd/ssd_server_upload.py
+++ examples/ssd/ssd_server_upload.py
@@ -17,6 +17,7 @@ model_weights = '../../models/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300
 ssd_server_detect = SsdDetectionServer(labelmap_file, model_def, model_weights)
 
 recepient_phone_number = 14088025434
+# recepient_phone_number = '+919443845253'
 
 def _send_sms_notification(to, message_body, callback_url):
     # Ensure that the env has the vaiables below defined
@@ -85,6 +86,42 @@ def detect_file():
     </form>
     '''
 
+
+@app.route('/curl/', methods=['POST'])
+def detect_curl_syntax():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        print 'file name {}'.format(file)
+        if file and allowed_file(file.filename):
+            print 'I believe we made it this far...'
+            filename = secure_filename(file.filename)
+            print '... and filename is {}'.format(filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            image = ssd_server_detect.load_image(UPLOAD_FOLDER + '/' + filename)
+            top_conf, top_label_indices, top_labels, \
+            top_xmin, top_ymin, top_xmax, top_ymax = ssd_server_detect.run_detect_net(image)
+
+            overlayed_file = UPLOAD_FOLDER + '/' + filename
+            ssd_server_detect.plot_boxes(overlayed_file, image,  
+                                         top_conf, top_label_indices, top_labels,                           
+                                         top_xmin, top_ymin, top_xmax, top_ymax)     
+
+            callback_url = request.base_url + 'notification/status/update'
+            print 'call back url {}'.format(callback_url)
+            message = 'Results' + ' ' + request.base_url.replace('/curl', '') + 'uploads/' + filename 
+            _send_sms_notification(recepient_phone_number,
+                                   message,
+                                   callback_url)
+
+            return redirect(url_for('detected_file',
+                                    filename=filename))
+    return "<p>Done!</p>"
+
+
 from flask import send_from_directory
 
 @app.route('/uploads/<filename>')
@@ -94,6 +131,16 @@ def detected_file(filename):
 
 @app.route('/detect/notification/status/update', methods=["POST"])
 def notification_delivery_status():
+    print "###Delivered  the notification"
+    return '''                                                                                                
+    <!doctype html> 
+    <title>Done</title>                                                                                
+    <h1>Done!</h1>                                                                    
+    '''
+
+
+@app.route('/curl/notification/status/update', methods=["POST"])
+def curl_notification_delivery_status():
     print "###Delivered  the notification"
     return '''                                                                                                
     <!doctype html> 
