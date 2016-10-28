824ed223084a0cc11048440e4c1e1f7cd4bd7da7
Auro Tripathy <ipserv@yahoo.com>
2016-10-28 10:23:24 -0700

    expanded the unit-test to test with two models, coco and VOC

--- examples/ssd/test_ssd_server_detect.py
+++ examples/ssd/test_ssd_server_detect.py
@@ -6,16 +6,34 @@ import os
 from pudb import set_trace
 import numpy as np
 import matplotlib.pyplot as plt
+from argparse import ArgumentParser
 
 # Unit test
 
-set_trace()
+parser = ArgumentParser()
 
-model_def = 'models/VGGNet/VOC0712/SSD_300x300/deploy.prototxt'
-model_weights = 'models/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300_iter_60000.caffemodel'
+# Add more options if you like
+parser.add_argument("-m", "--model", dest="model",
+                    help="pick from the coco or the VOC", required=True, choices=('coco', 'VOC'))
 
-# load VOC labels for the 21 classes
-labelmap_file = 'data/VOC0712/labelmap_voc.prototxt'
+try:
+    input = parser.parse_args()
+    model = input.model
+    print 'model is {}'.format(model)
+except IOError, msg:
+    parser.error(str(msg))
+
+if model == 'VOC':
+    model_def = 'models/VGGNet/VOC0712/SSD_300x300/deploy.prototxt'
+    model_weights = 'models/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300_iter_60000.caffemodel'
+    labelmap_file = 'data/VOC0712/labelmap_voc.prototxt'
+elif model == 'coco':
+    labelmap_file = 'data/coco/labelmap_coco.prototxt'
+    model_def = 'models/VGGNet/coco/SSD_300x300/deploy.prototxt'
+    model_weights = 'models/VGGNet/coco/SSD_300x300/VGG_coco_SSD_300x300_iter_240000.caffemodel'
+else:
+    print 'Unknown error'
+    exit(2)
 
 ssd_server_detect = SsdDetectionServer(labelmap_file, model_def, model_weights)
 
@@ -33,6 +51,11 @@ with open('unit-test.txt') as f:
         # set_trace()
         bounding_box_gt_list = read_annotations(expanduser('~') + '/data/VOCdevkit/' + gt_file)
         top_conf, top_label_indices, top_labels, top_xmin, top_ymin, top_xmax, top_ymax = ssd_server_detect.run_detect_net(image)
+
+        set_trace()
+
+        for c, l in zip(top_conf, top_labels): print 'conf {:04.2f} label {}'.format(c, l)
+
         ssd_server_detect.plot_boxes(os.path.basename(img_file), image, 
                                      top_conf, top_label_indices, top_labels, 
                                      top_xmin, top_ymin, top_xmax, top_ymax)
