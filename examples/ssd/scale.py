761b83c2738de406c22f8aac7038575d661d4cb5
Auro Tripathy <ipserv@yahoo.com>
2016-11-12 23:08:40 -0800

    initial

---
+++ examples/ssd/scale.py
@@ -0,0 +1,16 @@
+# http://www.willamette.edu/~gorr/classes/GeneralGraphics/Transforms/transforms2d.htm#Combining
+
+from shapely.geometry import Point
+from shapely.geometry import Polygon
+from shapely import affinity
+
+
+def scale_box(xmin, ymin, xmax, ymax, xfact, yfact):
+    rect = Polygon([(xmin, ymin), (xmin, ymax), (xmax, ymax), (xmax,ymin)])
+    rect_s = affinity.scale(rect, xfact=xfact, yfact=yfact)
+    # print rect_s.exterior.coords[:-1]
+    for coord in rect_s.exterior.coords[:-1]: 
+        print '{:06.2f} {:06.2f}'.format(coord[0], coord[1]) 
+    return  rect_s.exterior.coords[0][0], rect_s.exterior.coords[0][1], rect_s.exterior.coords[2][0], rect_s.exterior.coords[2][1] 
+
+
