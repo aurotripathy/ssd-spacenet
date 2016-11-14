e6b3bd3d017327dc6270e25ee732bbb61867cd0b
Auro Tripathy <ipserv@yahoo.com>
2016-11-13 22:33:56 -0800

    added origin=center for the scale call

--- examples/ssd/scale.py
+++ examples/ssd/scale.py
@@ -7,7 +7,8 @@ from shapely import affinity
 
 def scale_box(xmin, ymin, xmax, ymax, xfact, yfact):
     rect = Polygon([(xmin, ymin), (xmin, ymax), (xmax, ymax), (xmax,ymin)])
-    rect_s = affinity.scale(rect, xfact=xfact, yfact=yfact)
+    rect_s = affinity.scale(rect, xfact=xfact, yfact=yfact, origin='center')
+    # rect_s = affinity.scale(rect, xfact=xfact, yfact=yfact, origin=(0., 0.))
     # print rect_s.exterior.coords[:-1]
     for coord in rect_s.exterior.coords[:-1]: 
         print '{:06.2f} {:06.2f}'.format(coord[0], coord[1]) 
