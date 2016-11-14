622fa3ff29b4f4425b5eaf5d1887bd42aab7be6a
Auro Tripathy <ipserv@yahoo.com>
2016-11-13 23:32:52 -0800

    fixed numerator/demoninator issue

--- examples/ssd/test_scale.py
+++ examples/ssd/test_scale.py
@@ -1,13 +1,17 @@
 from scale import scale_box
 
-new_dim = (640, 360)
-current_dim = (500, 500)
-xfact = float(current_dim[0])/float(new_dim[0])
-yfact = float(current_dim[1])/float(new_dim[1])
-xmin = 5
-xmax = 10
-ymin = 5
-ymax = 10
+# new_dim = (640, 360)
+# current_dim = (500, 500)
+new_dim = (10, 10)
+current_dim = (5, 5)
+# xfact = float(current_dim[0])/float(new_dim[0])
+# yfact = float(current_dim[1])/float(new_dim[1])
+xfact = float(new_dim[0])/float(current_dim[0])
+yfact = float(new_dim[1])/float(current_dim[1])
+xmin = 1
+xmax = 2
+ymin = 1
+ymax = 2
 
 print 'x fact {:06.2f}, y fact {:06.2f}'.format(xfact, yfact)
 print '{:06.2f}, {:06.2f}, {:06.2f}, {:06.2f},'.format(xmin, ymin, xmax, ymax)
