cab836734ad8e3aad9d70cc64b04bf78ebd66a36
Auro Tripathy <ipserv@yahoo.com>
2017-01-29 17:07:09 -0800

    cleanup

--- examples/ssd/unit-tests/ut-imageClassPlusBB.py
+++ examples/ssd/unit-tests/ut-imageClassPlusBB.py
@@ -4,11 +4,13 @@
 #      -F "file=@/home/tempuser/Pictures/pup_litter.jpg"
 import cv2
 import json
+import random
+import ast
 from pudb import set_trace
+import os
 
 def execute_rest_api(img_name, outdir_path):
     url = "https://shatterline.ngrok.io/imageClassPlusBB/"
-    # img_name = '/home/tempuser/Pictures/two_pups.jpg'
     files = {'file':(img_name,  open(img_name, 'rb'), "Content-type: multipart/form-data")}
     import requests
     with open(img_name, 'rb') as f:
@@ -17,7 +19,6 @@ def execute_rest_api(img_name, outdir_path):
     print r.text
     s = r.text
 
-    import ast
     temp = ast.literal_eval(s)
     bb_list = ast.literal_eval(temp)
 
@@ -26,32 +27,34 @@ def execute_rest_api(img_name, outdir_path):
     cpimg = image.copy()
 
     COLORS = ((0,0,0), (51, 51, 255), (255, 51, 51), (51, 255, 51), (255,255,0), 
-              (0,255,255), (0,127,255), (128,0,255), (102,102,255), (255,102,102), (102,255,102) )
+              (0,255,255), (0,127,255), (128,0,255), (102,102,255), (255,102,102), 
+              (102,255,102) )
     tot_colors = len(COLORS)
 
-    import random
-
-
     for bb in bb_list:
-        print 'confidence: {}, (xmin, ymin): {}, (xmax, ymax): {}, label: {}'.format(bb['confidence'], 
-                                                                                     bb['x_min_y_min'], 
-                                                                                     bb['x_max_y_max'],
-                                                                                     bb['label'])
+        print 'confidence: {}, (xmin, ymin): {}, \
+        (xmax, ymax): {}, label: {}'.format(bb['confidence'], 
+                                            bb['x_min_y_min'], 
+                                            bb['x_max_y_max'],
+                                            bb['label'])
         (xmin, ymin) = bb['x_min_y_min']
         (xmax, ymax) = bb['x_max_y_max']
         label = bb['label']
         color_index = random.randint(0,tot_colors-1)
-        cv2.rectangle(cpimg, (xmin, ymin), (xmax, ymax), COLORS[color_index], 2)
-        cv2.putText(cpimg, label, (xmin, ymin + 15), cv2.FONT_HERSHEY_DUPLEX, 0.5, COLORS[color_index] , 1)
+        cv2.rectangle(cpimg, (xmin, ymin), (xmax, ymax), 
+                      COLORS[color_index], 2)
+        cv2.putText(cpimg, label, (xmin, ymin + 15), 
+                    cv2.FONT_HERSHEY_DUPLEX, 0.5, COLORS[color_index] , 1)
 
-        output_img = 'pup_litter_with_bb.jpg'
-        try:
-            cv2.imwrite(output_img,cpimg)
-        except ValueError:
-            print "Something went wrong"
+    output_img = os.path.join(outdir_path, os.path.splitext(os.path.basename(image_name))[0] + '_with_bb.jpg' )
+    print 'Writing superimposed image to {}'.format(output_img)
+    try:
+        cv2.imwrite(output_img, cpimg)
+    except ValueError:
+        print "Something went wrong"
 
 
-inputlist = open('../one-image.txt','r')
+inputlist = open('../frames.txt','r')
 lines = inputlist.readlines()
 for image_name in lines:
     image_name = image_name.replace('\n','')
